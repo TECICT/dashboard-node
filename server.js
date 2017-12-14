1d84aecaf18c47fd88deb93828fa98367ed3de50
laurensallart <laurens.allart@gmail.com>
2017-12-14 09:35:51 +0100

    change port number for heroku

--- server.js
+++ server.js
@@ -79,6 +79,6 @@ app.post('/video/upload', cors(), function(req, res) {
     });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on port 3000!')
 });
