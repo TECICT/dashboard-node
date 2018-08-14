8c5b9d55764884244ae122d1a881fa789909dfee
laurensallart <laurens.allart@gmail.com>
2018-08-14 10:02:35 +0200

    update listtype api

--- routes/listType.js
+++ routes/listType.js
@@ -4,13 +4,11 @@ var ListType = mongoose.model('ListType');
 var auth = require('./auth');
 
 // preload the params
-router.param('listName', function(req, res, next, listName) {
-  ListType.findOne({ listName: listName})
+router.param('listId', function(req, res, next, listId) {
+  ListType.findOne({ _id: listId})
     .then(function (list) {
       if (!list) { return res.sendStatus(404); }
-
       req.list = list;
-
       return next();
     }).catch(next);
 });
@@ -31,34 +29,26 @@ router.post('/', function(req, res) {
   })
 });
 
-router.put('/:listname', function(req, res) {
-  ListType.findOne(req.list)
-  .then(function(list) {
-    Object.keys(req.body).forEach(function(element) {
-      if (JSON.stringify(list[element]) !== JSON.stringify(req.body[element])) {
-        list[element] = req.body[element];
-      }
-    })
-    list.save().then(function(list) {
-      return res.json(list);
-    })
+router.put('/:listId', function(req, res) {
+  console.log(req.list);
+  Object.keys(req.body).forEach(function(element) {
+    if (JSON.stringify(req.list[element]) !== JSON.stringify(req.body[element])) {
+      req.list[element] = req.body[element];
+    }
   })
-});
-
-router.get('/:listName', function(req, res) {
-  ListType.findOne(req.list)
-  .then(function(list){
+  req.list.save().then(function(list) {
     return res.json(list);
   })
 });
 
-router.delete('/:listName', function(req, res) {
-  ListType.findOne(req.list)
-  .then(function(list){
-    list.remove()
-    .then(function() {
-      return res.sendStatus(204);
-    })
+router.get('/:listId', function(req, res) {
+  return res.json(req.list);
+});
+
+router.delete('/:listId', function(req, res) {
+  req.list.remove()
+  .then(function() {
+    return res.sendStatus(204);
   })
 });
 
